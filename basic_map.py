2d5a53feb038da767dbf9b2e6eb7a2b114431122
jpearl8 <31544850+jpearl8@users.noreply.github.com>
2019-03-30 11:44:23 -0400

    Update basic_map.py

--- basic_map.py
+++ basic_map.py
@@ -3,15 +3,14 @@ Pset 4a
 Makes basic map
 """
 
-import cv2
+
 import rospy
-import numpy as np
-import math
-from geometry_msgs.msg import Twist
+import random
 from math import radians
-from kobuki_msgs.msg import BumperEvent, Sound
-from sound_play.msg import SoundRequest
-from sound_play.libsoundplay import SoundClient
+import cv2
+import numpy as np
+from geometry_msgs.msg import Twist
+from kobuki_msgs.msg import BumperEvent, CliffEvent, WheelDropEvent
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 

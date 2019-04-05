88de20fd1b698b08c8ca684879e1aadaa6befd1c
Juliet Nwagwu Ume-Ezeoke <juliet@Juliets-MacBook-Pro.local>
2019-04-04 22:10:08 -0400

    int

--- b1_wander_test.py
+++ b1_wander_test.py
@@ -18,6 +18,10 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import Empty
 
+import map_util as mp
+import numpy as np
+import time
+
 
 
 def dist(pos1, pos2):
@@ -60,7 +64,22 @@ def angle_compare(curr_angle, goal_angle):
 class B1_Wander_Test:
     def __init__(self):
         #INITIALIZER: 
-        #TODO: needs all info about maps and such
+        # initializer for the maps 
+         # initialize MapDrawer object
+        self.mapObj = mp.MapDrawer(self.positionToMap)
+
+        # create blank array of negative ones to represent blank map 
+        self.my_map = -np.ones((30,40))
+
+        # Localization determined from EKF
+        # Position is a geometry_msgs/Point object with (x,y,z) fields
+        self.position = (3, 3) # just for testing purposes, reset to None later 
+        # Orientation is the rotation in the floor plane from initial pose
+        self.orientation = None
+
+        # handle obstacles
+        self.obstacle_pos = None
+        self.obstacle = False
 
         # speed and radians for turns set here
         self.lin_speed = 0.2  # m/s
@@ -111,6 +130,75 @@ class B1_Wander_Test:
             reset_odom.publish(Empty())
         # TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 5 Hz
         self.rate = rospy.Rate(5)
+
+    def positionToMap(self, position):
+        """
+        turn EKF position in meters into map position in coordinates
+        """
+        # ratio of world meters to map coordinates 
+        world_map_ratio = 0.2
+
+        # rang of meters for robot to travel in x and y direction (ie dimensions of that box), need to be condensed into 30, 40 map 
+        # position relative to 0,0 - so in steps? 
+        # convert meters to map coorinates -- need to edit -- commented out the right hand assertion 
+        step_x = int(position[0]/world_map_ratio)
+        step_y = int(position[1]/world_map_ratio)
+        if (position[0] != 0 and position != 1):
+            step_x = step_x + 0
+            step_y = step_y + 0
+        
+        return (step_x , step_y)
+
+    def initializeMap(self):
+        # first map update, need to do twice because it doesn't show up nicely the first time .p
+        self.mapObj.UpdateMapDisplay(self.my_map, (0, 0))
+        self.mapObj.UpdateMapDisplay(self.my_map, (0, 0))
+        # show map for this amount of time 
+        time.sleep(0.25)
+
+        # next map update - zeroing out four corners of the map relative to current position -- not nessecary will change 
+        start_pos = (0, 0)
+        start_pos_map = self.positionToMap(start_pos)
+        print start_pos_map
+        self.my_map[start_pos_map[0], start_pos_map[1]] = 0
+        self.my_map[start_pos_map[0]-1, start_pos_map[1]] = 0
+        self.my_map[start_pos_map[0]-1, start_pos_map[1]-1] = 0
+        self.my_map[start_pos_map[0], start_pos_map[1]-1] = 0
+        #print my_map
+        self.mapObj.UpdateMapDisplay(self.my_map, start_pos)
+        time.sleep(0.25)
+
+    def updateMapFree(self):
+        # update map with current position and knowlege that this position is free
+        current_pos = self.position
+        current_pos_map = self.positionToMap(current_pos)
+
+        # check that current pos in the map is within the bounds
+        if (current_pos_map[0] <= 300 and current_pos_map[0] >= 0 and current_pos_map[1] <= 400 and current_pos_map[1] >= 0):
+                # if the current position is ok, set it to be free and update and show the map 
+                self.my_map[current_pos_map[0], current_pos_map[1]] = 0
+                self.mapObj.UpdateMapDisplay(self.my_map, current_pos)
+                print "map updated"
+                time.sleep(0.5)            
+        else:
+            # if the current position is not ok, let it be known that the values are off, do not change the map array
+            print "values are off! current map pos: %d, %d" % (current_pos_map[0], current_pos_map[1])
+
+    def updateMapOccupied(self):
+        # update map with position of obstacle and knowledge that that position will be occupied 
+        obstacle_pos = self.obstacle_pos
+        obstacle_pos_map = self.positionToMap(obstacle_pos)
+
+        # check that obstacle pos in the map is ok
+        if (current_pos_map[0] <= 30 and current_pos_map[0] >= 0 and current_pos_map[1] <= 40 and current_pos_map[1] >= 0):
+                # if the obstacle position is ok, set it to be occupied
+                self.my_map[current_pos_map[0], obstacle_pos_map[1]] = 1
+                # show the map, but still relative to current position 
+                self.mapObj.UpdateMapDisplay(self.my_map, current_pos)
+                time.sleep(3)
+        else:
+            # if the current position is not ok, let it be known that the values are off, do not change the map array
+            print "values are off! obstacle map pos: %d, %d" % (obstacle_pos_map[0], obstacle_pos_map[1])
       
     def wander(self):
         """
@@ -199,9 +287,14 @@ class B1_Wander_Test:
             rospy.loginfo("HERE")
             move_cmd.linear.x = self.lin_speed
             move_cmd.angular.z = 0
-            rospy.loginfo('({:.2f}, {:.2f})\t{:.1f} deg'.format(
-                self.position[0], self.position[1], math.degrees(self.orientation)))
+            # rospy.loginfo('({:.2f}, {:.2f})\t{:.1f} deg'.format(
+            #     self.position[0], self.position[1], math.degrees(self.orientation)))
+
+            rospy.loginfo('%d, %d'.format(
+                    self.position[0], self.position[1]))
             current_pos = (self.position[0], self.position[1])
+
+            self.updateMapFree()
                         
             # publish the velocity
             self.cmd_vel.publish(move_cmd)

e5cb29c8d57535df71780f30b8200208149824df
Julia Pearl <jpearl@college.harvard.edu>
2019-04-12 08:28:02 -0400

    obs

--- twelve.py
+++ twelve.py
@@ -24,6 +24,37 @@ import time
 
 
 
+def centroid(contour):
+        """
+        Compute the (x,y) centroid position of the counter
+        :param contour: OpenCV contour
+        :return: Tuple of (x,y) centroid position
+        """
+
+        def centroid_x(c):
+            """
+            Get centroid x position
+            :param c: OpenCV contour
+            :return: x position or -1
+            """
+            M = cv2.moments(c)
+            if M['m00'] == 0:
+                return -1
+            return int(M['m10'] / M['m00'])
+
+        def centroid_y(c):
+            """
+            Get centroid y position
+            :param c: OpenCV contour
+            :return: y position or -1
+            """
+            M = cv2.moments(c)
+            if M['m00'] == 0:
+                return -1
+            return int(M['m01'] / M['m00'])
+
+        return centroid_x(contour), centroid_y(contour)
+
 
 
 class B1_Wander_Test:
